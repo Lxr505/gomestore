29d22c9fb7502ac67724a7fe8b9af8a2574aab2f
lixuran <823835197@qq.com>
2020-03-10 02:05:00 +0800

    environment

---
+++ gomeoline/gulpfile.js
@@ -0,0 +1,65 @@
+const gulp=require("gulp");
+const connect=require("gulp-connect");
+const sass=require("gulp-sass");
+const  babel=require("gulp-babel");
+const sourcemaps=require("gulp-sourcemaps");
+
+gulp.task("html",done=>{
+
+    gulp.src("*.html")
+    .pipe(gulp.dest("dist"))
+    .pipe(connect.reload());
+    done();
+});
+
+gulp.task("sass",done=>{
+    gulp.src("sass/*.scss")
+    .pipe(sourcemaps.init())
+    .pipe(sass({"outputStyle":"compact"}))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest("dist/css"))
+    .pipe(connect.reload());
+    done();
+});
+
+gulp.task("babel",done=>{
+    gulp.src("js/*.js")
+    .pipe(babel({"presets":["@babel/env"]}))
+    .pipe(gulp.dest("dist/js"))
+    .pipe(connect.reload());
+    done();  
+});
+
+gulp.task("libs",done=>{
+    gulp.src("libs/*.js")
+    .pipe(gulp.dest("dist/libs"));
+    done();
+});
+
+gulp.task("server",done=>{
+
+    connect.server(
+        {
+            root:"dist",
+            livereload:true
+        })
+    done();
+});
+
+gulp.task("imgs",done=>{
+    gulp.src("imgs/**")
+    .pipe(gulp.dest("dist/imgs"));
+    done();
+});
+
+gulp.task("watch",done=>{
+
+    gulp.watch(["*.html","sass/*.scss","js/*.js","libs/*.js","imgs/**"],gulp.series
+    ("html","sass","babel","libs","imgs"));
+
+
+    done();
+});
+
+
+gulp.task("default",gulp.parallel("server","watch"));
